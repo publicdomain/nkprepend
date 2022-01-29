27225d4b8ff6a5de7139d393b2c95e50c0e986cd
Victor <publicdomainvic@gmail.com>
2022-01-29 17:47:42 -0400

    Add project files

---
+++ nkPrepend/MainForm.cs
@@ -0,0 +1,115 @@
+﻿// <copyright file="MainForm.cs" company="PublicDomainWeekly.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace nkPrepend
+{
+    // Directives
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Ons the new tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnNewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the options tool strip menu item drop down item clicked.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the free releases public domainis tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the original thread donation codercom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the source code githubcom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the on about tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnOnAboutToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the file list view drag enter.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnFileListViewDragEnter(object sender, DragEventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the file list view item drag.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnFileListViewItemDrag(object sender, ItemDragEventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Ons the exit tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+    }
+}
