1bafbc0b69fe8a52bd184d972e9de3cf8e8bc915
Victor <publicdomainvic@gmail.com>
2022-01-31 02:43:30 -0400

    Add OnFreeReleasesPublicDomainisToolStripMenuItemClick()

--- nkPrepend/MainForm.cs
+++ nkPrepend/MainForm.cs
@@ -89,7 +89,8 @@ namespace nkPrepend
         /// <param name="e">Event arguments.</param>
         private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open our website
+            Process.Start("https://publicdomain.is");
         }
 
         /// <summary>
